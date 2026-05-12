5bcb386226e7cf88993167fd7e61662d9d5b4adb
dlq <darcy.quesnel@gmail.com>
2026-05-12 13:13:02 -0400

    Initial app scaffold

---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parliaments
+//
+//  Created by Darcy Quesnel on 2026-05-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/ParliamentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParliamentsApp.swift
+//  Parliaments
+//
+//  Created by Darcy Quesnel on 2026-05-12.
+//
+
+import SwiftUI
+
+@main
+struct ParliamentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
